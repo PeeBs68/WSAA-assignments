650a015e03668f6207131497c5c8003f9075bd52
Phelim Barry <phelimb@yahoo.com>
2024-02-01 23:44:20 +0000

    Initial Commit

---
+++ currentweather.py
@@ -0,0 +1,7 @@
+# currentweather.py
+
+# Author: Phelim Barry
+
+# Purpose: outputs the current temperature to the console (and maybe the current wind direction, for extra marks)
+
+# Use latitude = 51.734507 & longitude = -8.539811 (Kinsale Golf Club!)
