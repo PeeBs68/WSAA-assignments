ba66d48556c9476dfa967b86821a02f6b46f3b85
Phelim Barry <phelimb@yahoo.com>
2024-02-06 21:16:30 +0000

    Working on Assignment1

--- currentweather.py
+++ currentweather.py
@@ -4,4 +4,28 @@
 
 # Purpose: outputs the current temperature to the console (and maybe the current wind direction, for extra marks)
 
-# Use latitude = 51.734507 & longitude = -8.539811 (Kinsale Golf Club!)
+# Using latitude = 51.734507 & longitude = -8.539811 (Kinsale Golf Club, Co. Cork!)
+
+import json
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=51.734507&longitude=-8.539811&current=temperature_2m&current=wind_direction_10m"
+
+response = requests.get(url)
+data = response.json()
+
+current_wdir = data["current"]["wind_direction_10m"]
+current_temp = data["current"]["temperature_2m"]
+
+# Just to add a bit more detail
+# https://uni.edu/storm/Wind%20Direction%20slide.pdf
+if current_wdir < 90:
+    wdir = "North/Easterly"
+elif current_wdir < 180:
+    wdir = "South/Easterly"
+elif current_wdir < 279:
+    wdir = "West/Southerly"
+else:
+    wdir = "North/Westerly"
+
+print(f"Right now at Kinsale Golf Club the temperature is {current_temp} degrees celcius and the wind direction is {current_wdir} degrees (which means it is roughly {wdir})")
