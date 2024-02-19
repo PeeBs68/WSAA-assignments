c2c9ea411b73b7d7bb221d0c0793d5df8373efef
Phelim Barry <phelimb@yahoo.com>
2024-02-19 20:58:40 +0000

    Working on assignment3

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -2,4 +2,20 @@
 
 # Author: Phelim Barry
 
-# Purpose: xxx
+# Purpose: retrieves the "exchequer account (historical series)"" from cso.ie and stores it in a json file"
+
+import requests
+import json
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def getdata():
+    response = requests.get(url)
+    data = response.json()
+    print(response) #200 is good
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(data), file=fp)
+
+getdata()
+
+#N ote to self - use Shift+Alt+F to reformat JSON file
