452afb2d971890cb0e05847884fcd02f4415c739
Phelim Barry <phelimb@yahoo.com>
2024-02-25 00:03:30 +0000

    Adding some placeholders for assignment4

--- assignment04-github.py
+++ assignment04-github.py
@@ -4,10 +4,32 @@
 
 # Purpose: Read a file from a repo, make changes to the file and then push it back to the repo
 
-# Something along theses lines for starters...
 import requests
 import json
 
+# define old and new strings
+original_name = "Andrew"
+new_name = "Phelim"
+
+# pull the file from the repo...
+#
+#
+
+# read in the file and make the change
+# https://www.geeksforgeeks.org/how-to-search-and-replace-text-in-a-file-in-python/
+with open('filename', 'r') as fp:
+    data = fp.read()
+    data = data.replace(original_name, new_name)
+
+# save the changes to the file
+with open('filename', 'w') as fp:
+    fp.write(data)
+
+# write the file back to the repo...
+#
+#
+
+# Will probably need this for the writing back to GitHub
 from config import config as cfg
 url = 'https://api.github.com/repos/PeeBs68/aprivateone'
 
